5280e1984015b768814db91006b04e3a0fe1adc8
Krishnapavan25 <pavankasi25@gmail.com>
2024-05-27 17:49:06 +0530

    Add the files

---
+++ MyDate.java
@@ -0,0 +1,15 @@
+class MyDate
+{
+  private int day,month,year;
+
+  public void initDate()
+  {
+     day=19;
+     month=03;
+     year=2024;
+   }
+   public void dispDate()
+   {
+   System.out.println("Date is:"+day+"/"+month+"/"+year);
+   }
+}
