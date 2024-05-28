a35c1939b33b81114cf82dd3f7d2bb66f67e9efe
kapavan <pavankasi25@gmail.com>
2024-05-28 16:27:30 +0530

    adding new files

---
+++ DateMainexp.java
@@ -0,0 +1,13 @@
+class DateMain
+{
+ public static void main(String[] args)
+  {
+
+  //
+  MyDateexp d1 = new MyDateexp();
+  d1.dispDate();
+
+  MyDateexp d2 = new MyDateexp(17,4,2024);
+  d2.dispDate();
+  }
+}
